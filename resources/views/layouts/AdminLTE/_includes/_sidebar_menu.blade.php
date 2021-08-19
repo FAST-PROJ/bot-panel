7a753e87fdf1284e3616ee7c6aed682a4607aecc
ovalves <vinicius_o.a@live.com>
2021-08-19 14:02:57 -0300

    Fix: Renomeando as views de layout, ajustes nas imagens

--- resources/views/layouts/AdminLTE/_includes/_sidebar_menu.blade.php
+++ resources/views/layouts/AdminLTE/_includes/_sidebar_menu.blade.php
@@ -9,7 +9,7 @@
 				<a href="{{ route('home') }}" title="Dashboard"><i class="fa fa-dashboard"></i> <span> DASHBOARD</span></a>
 			</li>
 
-            @if (Auth::user()->isStudent())
+            @if (Auth::user()?->isStudent())
                 <li class="{{ Request::segment(1) === 'bot' ? 'active' : null }}">
                     <a href="{{ route('bot.tinker') }}" title="Bot Professor Virtual"><i class="fa fa-android"></i> <span> PROFESSOR VIRTUAL</span></a>
                 </li>
@@ -41,36 +41,36 @@
                 </li>
 			@endif
 
-			<li class="treeview
-				{{ Request::segment(1) === 'config' ? 'active menu-open' : null }}
-				{{ Request::segment(1) === 'user' ? 'active menu-open' : null }}
-				{{ Request::segment(1) === 'role' ? 'active menu-open' : null }}
-				">
-				<a href="#">
-					<i class="fa fa-gear"></i>
-					<span>CONFIGURAÇÕES</span>
-					<span class="pull-right-container">
-						<i class="fa fa-angle-left pull-right"></i>
-					</span>
-				</a>
-				<ul class="treeview-menu">
-					@if (Auth::user()->can('root-dev', ''))
-						<li class="{{ Request::segment(1) === 'config' && Request::segment(2) === null ? 'active' : null }}">
-							<a href="{{ route('config') }}" title="App Config">
-								<i class="fa fa-gear"></i> <span> CONFIGURAÇÕES DO APP</span>
-							</a>
-						</li>
-					@endif
-					<li class="
-						{{ Request::segment(1) === 'user' ? 'active' : null }}
-						{{ Request::segment(1) === 'role' ? 'active' : null }}
-						">
-						<a href="{{ route('user') }}" title="Users">
-							<i class="fa fa-user"></i> <span> USUÁRIOS</span>
-						</a>
-					</li>
-				</ul>
-			</li>
+            @if (Auth::user()?->can('root', ''))
+                <li class="treeview
+                    {{ Request::segment(1) === 'config' ? 'active menu-open' : null }}
+                    {{ Request::segment(1) === 'user' ? 'active menu-open' : null }}
+                    {{ Request::segment(1) === 'role' ? 'active menu-open' : null }}
+                    ">
+                    <a href="#">
+                        <i class="fa fa-gear"></i>
+                        <span>CONFIGURAÇÕES</span>
+                        <span class="pull-right-container">
+                            <i class="fa fa-angle-left pull-right"></i>
+                        </span>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="{{ Request::segment(1) === 'config' && Request::segment(2) === null ? 'active' : null }}">
+                            <a href="{{ route('config') }}" title="App Config">
+                                <i class="fa fa-gear"></i> <span> CONFIGURAÇÕES DO APP</span>
+                            </a>
+                        </li>
+                        <li class="
+                            {{ Request::segment(1) === 'user' ? 'active' : null }}
+                            {{ Request::segment(1) === 'role' ? 'active' : null }}
+                            ">
+                            <a href="{{ route('user') }}" title="Users">
+                                <i class="fa fa-user"></i> <span> USUÁRIOS</span>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            @endif
 		</ul>
 	</section>
 </aside>
