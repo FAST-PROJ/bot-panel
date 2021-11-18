7de60eeb201f852fa3077a49cdbc193fc8b29c21
ovalves <vinicius_o.a@live.com>
2021-11-18 15:16:37 -0300

    Feat: Criando um endpoint para salvar as respostas da IA

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Answer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +18,29 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/answer', function (Request $request) {
+    try {
+        Answer::create([
+            'user_id' => $request->input('id'),
+            'answer' => $request->input('answer'),
+        ]);
+
+        return Response::json(
+            [
+                'message' => 'Resposta salva com sucesso',
+                'status' => true,
+            ],
+            200,
+        );
+    } catch (\Throwable) {
+        return Response::json(
+            [
+                'message' => 'Erro ao salvar a resposta',
+                'status' => false,
+            ],
+            400,
+        );
+    }
 });
