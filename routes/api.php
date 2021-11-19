0765cc2353c8ddf5c81cd2e9f835f3fba084039b
ovalves <vinicius_o.a@live.com>
2021-11-18 21:55:36 -0300

    Fix: Ajuste no endpoint de respostas da api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Answer;
+use App\Models\User;
+use App\Notifications\BotNotification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
@@ -22,10 +24,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/answer', function (Request $request) {
     try {
-        Answer::create([
-            'user_id' => $request->input('id'),
+        $userId = $request->input('id');
+
+        $answerID = Answer::create([
+            'user_id' => $userId,
+            'question' => $request->input('question'),
             'answer' => $request->input('answer'),
         ]);
+
+        $user = User::find($userId);
+        $user->notify(new BotNotification($user, $answerID));
 
         return Response::json(
             [
@@ -34,7 +42,7 @@ Route::post('/answer', function (Request $request) {
             ],
             200,
         );
-    } catch (\Throwable) {
+    } catch (\Throwable $e) {
         return Response::json(
             [
                 'message' => 'Erro ao salvar a resposta',
