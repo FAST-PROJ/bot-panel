905be6dab65d14ad63ee8ce09253b26994ebbae3
ovalves <vinicius_o.a@live.com>
2021-11-18 21:56:09 -0300

    Feat: Adição das rotas de notification e subscription e perguntas e respostas

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\ConfigController;
 use App\Http\Controllers\Error\ErrorController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\Profile\ProfileController;
+use App\Http\Controllers\PushSubscriptionController;
+use App\Http\Controllers\User\AnswerController;
 use App\Http\Controllers\User\RoleController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\VirtualTeacherController;
@@ -52,12 +55,31 @@ Route::group(['namespace' => 'App\Http\Controllers\User'], function () {
     // Estudante
     Route::get('/my/courses', [UserController::class, 'index'])->name('student.courses');
     Route::get('/my/learning/plan', [UserController::class, 'index'])->name('student.learning.plan');
+
+    // Respostas
+    Route::get('/user/answers', [AnswerController::class, 'index'])->name('user.answers');
+    Route::get('/user/answers/show/{id}', [AnswerController::class, 'show'])->name('user.answers.show');
 });
 
 // Bot Routes
 Route::match(['get', 'post'], '/botman', [VirtualTeacherController::class, 'handle']);
 Route::get('/bot/tinker', [VirtualTeacherController::class, 'tinker'])->name('bot.tinker');
 
-// push notifications
-Route::get('/push', 'PushController@push')->name('push');
-Route::post('/push', 'PushController@store');
+// Push notifications
+Route::post('notifications', [NotificationController::class, 'store']);
+Route::get('notifications', [NotificationController::class, 'index']);
+Route::patch('notifications/{id}/read', [NotificationController::class, 'markAsRead']);
+Route::post('notifications/mark-all-read', [NotificationController::class, 'markAllRead']);
+Route::post('notifications/{id}/dismiss', [NotificationController::class, 'dismiss']);
+
+// Push Subscriptions
+Route::post('subscriptions', [PushSubscriptionController::class, 'update']);
+Route::post('subscriptions/delete', [PushSubscriptionController::class, 'destroy']);
+
+// Manifest file (optional if VAPID is used)
+Route::get('manifest.json', function () {
+    return [
+        'name' => config('app.name'),
+        'gcm_sender_id' => config('webpush.gcm.sender_id'),
+    ];
+});
