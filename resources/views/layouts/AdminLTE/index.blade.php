0b7c0063281cc277efcd95a649cdd91abd302b69
ovalves <vinicius_o.a@live.com>
2021-11-18 21:55:14 -0300

    Fix: Ajuste no template da app

--- resources/views/layouts/AdminLTE/index.blade.php
+++ resources/views/layouts/AdminLTE/index.blade.php
@@ -4,54 +4,57 @@
         @include('layouts.AdminLTE._includes._head')
     </head>
     <body class="hold-transition skin-{{ \App\Models\Config::find(1)->skin }} {{ \App\Models\Config::find(1)->layout }} sidebar-mini">
-        <div class="wrapper">
+        <div id="app">
+            <div class="wrapper">
 
-            @include('layouts.AdminLTE._includes._header_menu')
+                @include('layouts.AdminLTE._includes._header_menu')
 
-            @if(Auth::check())
-                @include('layouts.AdminLTE._includes._sidebar_menu')
-            @endif
-
-            <div class="content-wrapper">
-                <nav class="navbar navbar-static-top">
-                    <div class="navbar-header">
-                        <a href="" class="navbar-brand" id="" style="color:#222d32;"><i class="fa fa-@yield('icon_page')"></i> @yield('title')</a>
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2" aria-expanded="false">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </div>
-                    <div class="navbar-collapse collapse" id="navbar-collapse-2" aria-expanded="false" style="height: 1px;">
-                        <ul class="nav navbar-nav">
-
-                            @yield('page_menu')
-
-                        </ul>
-                    </div>
-                </nav>
-
-                @if(Session::has('flash_message'))
-
-                    <div class="{{ Session::get('flash_message')['class'] }}" style="padding: 10px 20px;" id="flash_message">
-                        <div style="color: #fff; display: inline-block; margin-right: 10px;">
-                            {!! Session::get('flash_message')['msg'] !!}
-                        </div>
-                    </div>
-
+                @if(Auth::check())
+                    @include('layouts.AdminLTE._includes._sidebar_menu')
                 @endif
 
-                <section class="content">
-                    <div class="row">
-                        <div class="col-md-12">
-
-                            @yield('content')
-
+                <div class="content-wrapper">
+                    <nav class="navbar navbar-static-top">
+                        <div class="navbar-header">
+                            <a href="" class="navbar-brand" id="" style="color:#222d32;"><i class="fa fa-@yield('icon_page')"></i> @yield('title')</a>
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-2" aria-expanded="false">
+                                <i class="fa fa-bars"></i>
+                            </button>
                         </div>
-                    </div>
-                </section>
+                        <div class="navbar-collapse collapse" id="navbar-collapse-2" aria-expanded="false" style="height: 1px;">
+                            <ul class="nav navbar-nav">
+
+                                @yield('page_menu')
+
+                            </ul>
+                        </div>
+                    </nav>
+
+                    @if(Session::has('flash_message'))
+
+                        <div class="{{ Session::get('flash_message')['class'] }}" style="padding: 10px 20px;" id="flash_message">
+                            <div style="color: #fff; display: inline-block; margin-right: 10px;">
+                                {!! Session::get('flash_message')['msg'] !!}
+                            </div>
+                        </div>
+
+                    @endif
+
+                    <section class="content">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                @yield('content')
+
+                            </div>
+                        </div>
+                    </section>
+
+                </div>
+
+                @include('layouts.AdminLTE._includes._footer')
 
             </div>
-
-            @include('layouts.AdminLTE._includes._footer')
 
         </div>
 
